1b021db3c5452c3a0a27512da56cb771027cbf70
tj <147413154+tomjmarz1@users.noreply.github.com>
2024-01-30 09:05:30 -0600

    ch

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -17,13 +17,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
-
-@app.route('/', strict_slashes=False)
-def index():
-    """Route for `/`"""
-    return render_template('4-index.html')
-
-
 @babel.localeselector
 def get_locale():
     """Retrieves locale from request"""
@@ -31,6 +24,14 @@ def get_locale():
     if locale and locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Route for `/`"""
+    return render_template('4-index.html')
+
+
 
 
 if __name__ == '__main__':
