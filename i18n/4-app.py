30d6f73023b58bb6e9aec90852ebf491eddc0de8
tj <147413154+tomjmarz1@users.noreply.github.com>
2024-01-31 12:07:08 -0600

    test

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -31,9 +31,6 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-# babel.init_app(app, locale_selector=get_locale)
-
-
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """ Return index.html template """
