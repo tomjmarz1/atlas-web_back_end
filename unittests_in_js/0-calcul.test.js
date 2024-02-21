fa88cf4fd22709809831c46bbe2b7a79aed088be
tj <147413154+tomjmarz1@users.noreply.github.com>
2024-02-21 17:06:43 -0600

    hello

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -4,7 +4,9 @@ var assert = require('assert');
 
 describe('calculateNumber', function () {
   it('should return result of adding rounded ', function () {
+    assert.equal(calculateNumber(1.0, 3.2), 4);
     assert.equal(calculateNumber(1.2, 3.2), 4);
+    assert.equal(calculateNumber(1.7, 3.2), 5);
   });
   it('should return result of adding rounded numbers', function () {
     assert.equal(calculateNumber(1.2, 3.7), 5);
