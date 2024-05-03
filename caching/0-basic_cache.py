85d6faf748b345a255ab1c4e77c1a633a3c50ad7
tj <147413154+tomjmarz1@users.noreply.github.com>
2024-05-03 11:12:52 -0500

    asdffasdf

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -5,14 +5,14 @@
 BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
-  """functions like a basic cache"""
-  def put(self, key, item):
     """functions like a basic cache"""
-    self.cache_data[key] = item
+    def put(self, key, item):
+        """functions like a basic cache"""
+        self.cache_data[key] = item
 
-  def get(self, key):
-    """functions like a basic cache"""
-    if key and key in self.cache_data:
-      return self.cache_data[key]
-    else:
-      return None
+    def get(self, key):
+        """functions like a basic cache"""
+        if key and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
